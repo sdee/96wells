122ce0563ef873060c3db137728776b7d526f727
Sutee Dee <thaiyoshi@gmail.com>
2017-04-21 00:27:01 -0700

    constants

--- client/src/reducers/app.js
+++ client/src/reducers/app.js
@@ -1,7 +1,15 @@
+
+const layouts = ['listorder', 'random']
+
+const sizes = {
+	'96': [8,12],
+	'284': [24,16]
+}
+
 const initialState = {
-	size: '96',
-	datasource: 'test1',
-	layout: 'default'
+	size: '96', //number of wells
+	datasource: 'test1', //source of sample list
+	layout: 'listorder' //algorithm for placing samples in wells
 };
 
 const app = (state = initialState, action) => {
