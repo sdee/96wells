c46bd095773e9fbdc39a0f23d85bf36b9b34b593
Sutee Dee <thaiyoshi@gmail.com>
2017-04-28 13:36:36 -0700

    simple text

--- client/src/components/PlateLayout/Well.jsx
+++ client/src/components/PlateLayout/Well.jsx
@@ -11,6 +11,7 @@ class Well extends Component {
 			<g transform="translate(55,55)">
 			<circle r="25" cx={this.props.i*55} cy={this.props.j*55} fill={this.props.color}>
 			</circle>
+			<text x={this.props.i*55-12.5} y={this.props.j*55} font-family="sans-serif" text-anchor="middle" font-size="13px">test</text>
 		</g>
 		);
 	}
