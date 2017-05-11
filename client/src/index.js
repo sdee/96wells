267a382a24c1b2d0ee41f81ecf9990b64624acf6
Sutee Dee <thaiyoshi@gmail.com>
2017-05-11 16:14:16 -0700

    adds hook for developer tools

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { Provider } from 'react-redux';
 import App from './App';
@@ -10,9 +10,14 @@ import thunk from 'redux-thunk';
 import rootReducer from './reducers/index';
 import './index.css';
 
-const store = createStore(rootReducer,
-	composeWithDevTools(applyMiddleware(thunk))
-);
+// const store = createStore(rootReducer,
+// 	composeWithDevTools(applyMiddleware(thunk))
+// );
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, /* preloadedState, */ composeEnhancers(
+    applyMiddleware(thunk)
+  ));
 
 const MaterialApp = () => (
 	<MuiThemeProvider>
