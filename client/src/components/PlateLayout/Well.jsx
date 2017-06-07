5cf8dd0cd943cbaea636508d6a6cde1829d171c7
Sutee Dee <thaiyoshi@gmail.com>
2017-06-07 14:20:37 -0700

    Well as DropTarget and DragSource

--- client/src/components/PlateLayout/Well.jsx
+++ client/src/components/PlateLayout/Well.jsx
@@ -1,30 +1,57 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { DragSource } from 'react-dnd';
+import { DragSource, DropTarget } from 'react-dnd';
 
 const wellSource = {
 	beginDrag(props) {
 		return {
 			id: props.id,
 			row: props.i,
-			col: props.j
-		}
+			col: props.j,
+			coord: [props.i, props.j]
+		};
 	}
 };
 
-function collect(connect, monitor) {
-  return {
-    connectDragSource: connect.dragSource(),
-    isDragging: monitor.isDragging(),
+const wellTarget = {
+	hover(props, monitor, component) {
+	const dragCoord = monitor.getItem().coord;
+	const hoverCoord = [props.i, props.j];
+
+	 console.log("hover "+hoverCoord);
+	 // Don't replace items with themselves
+	 if (dragCoord === hoverCoord) {
+		return;
+	 }
+	 //
+	//  props.moveCard(dragCoord, hoverCoord);
+	 monitor.getItem().coord = hoverCoord;
+ }
+};
+
+function dragCollect(connect, monitor) {
+	return {
+		connectDragSource: connect.dragSource(),
+		isDragging: monitor.isDragging(),
 		connectDragPreview: connect.dragPreview()
-  };
+	};
 }
 
-class Well extends Component {
+function dropCollect(connect, monitor) {
+	return {
+		connectDropTarget: connect.dropTarget()
+	};
+};
+
+const DragSourceDecorator = DragSource('Well', wellSource, dragCollect);
+
+const DropTargetDecorator = DropTarget('Well', wellTarget, dropCollect);
+
+class SVGWell extends Component {
 
 	constructor(props) {
 		super();
-	};
+	}
 
 	render() {
 		const i = this.props.i;
@@ -32,6 +59,7 @@ class Well extends Component {
 		const color = this.props.color;
 		const connectDragSource = this.props.connectDragSource;
 		const connectDragPreview = this.props.connectDragPreview;
+		const connectDropTarget = this.props.connectDropTarget;
 		const isDragging = this.props.isDragging;
 		const spacing = 73;
 		const opacity = 0.4;
@@ -41,7 +69,7 @@ class Well extends Component {
 
 		return connectDragPreview(
 			<g>
-				{connectDragSource(
+				{connectDragSource(connectDropTarget(
 					<g transform="translate(55,55)">
 						{ j === 0 ? (
 							<text
@@ -64,29 +92,30 @@ class Well extends Component {
 							cursor="cell"
 							fillOpacity={isDragging ? opacity / 2 : opacity}
 						/>
-					<text
-						x={i * spacing}
-						y={45 + j * spacing}
-						fontFamily="helvetica"
-						textAnchor="middle"
-						fontSize="12px"
-						fontWeight="bold"
-					>
+						<text
+							x={i * spacing}
+							y={45 + j * spacing}
+							fontFamily="helvetica"
+							textAnchor="middle"
+							fontSize="12px"
+							fontWeight="bold"
+						>
 						{this.props.labels.map((l, x) => <tspan x={5 + i * spacing} dy={`${(0.2 + (x * 0.9)).toString()}em`} key={'text' + i + '_' + j + l}>{l}</tspan>)}
 					</text>
 					</g>
-		)
+		))
 	}
 			</g>
 	);
 	}
 }
 
-Well.propTypes = {
+SVGWell.propTypes = {
 	i: PropTypes.number,
 	j: PropTypes.number,
 	labels: PropTypes.array,
 	color: PropTypes.string
 };
 
-export default DragSource("Well", wellSource, collect)(Well);
+const Well = DropTargetDecorator(DragSourceDecorator(SVGWell));
+module.exports = Well;
